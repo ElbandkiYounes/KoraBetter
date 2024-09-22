247fe95c80ddcd9f0bf66ea550f154e3c9b90525
EL BANDKI YOUNES <youneselbandki0@gmail.com>
2024-09-22 18:38:23 +0100

    api change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'https://football-highlights-api.p.rapidapi.com',
+    apiBaseUrl: 'https://sport-highlights-api.p.rapidapi.com/football',
     apiHostName: 'x-rapidapi-host',
     apiHostValue: 'football-highlights-api.p.rapidapi.com',
     apiKeyName: 'x-rapidapi-key',
